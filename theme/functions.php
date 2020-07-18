d68d38a1a47db37760eb0a2ee6f5990560ddc72e
Dimitri <dimitri@MacBook-Pro.local>
2020-07-18 18:14:10 +0200

    remove linting, minor changes

--- theme/functions.php
+++ theme/functions.php
@@ -122,12 +122,12 @@ add_action( 'widgets_init', 'agn_theme_widgets_init' );
 function agn_theme_scripts() {
 
 	// Loads required CSS header only.
-	wp_enqueue_style( '_s-style', get_stylesheet_uri() );
+	wp_enqueue_style( 'agn-theme-style', get_stylesheet_uri() );
 
 	// Loads bundled frontend CSS.
-	wp_enqueue_style( '_s-frontend-styles', get_stylesheet_directory_uri() . '/public/frontend.css' );
+	wp_enqueue_style( 'agn-theme-frontend-styles', get_stylesheet_directory_uri() . '/public/frontend.css' );
 
-	wp_enqueue_script( '_s-frontend-scripts', get_template_directory_uri() . '/public/frontend-bundle.js', array(), null, true );
+	wp_enqueue_script( 'agn-theme-frontend-scripts', get_template_directory_uri() . '/public/frontend-bundle.js', array(), null, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );

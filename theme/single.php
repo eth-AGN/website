17c99fb9b84f140a41079ae7d0f4ed7cea4b110b
Dimitri <dimitri@MacBook-Pro.local>
2020-08-29 00:54:04 +0200

    add handeln post layout

--- theme/single.php
+++ theme/single.php
@@ -16,10 +16,17 @@ get_header();
 		<?php
 		while ( have_posts() ) :
 			the_post();
+			if (in_category('wissen')) {
+				get_template_part('template-parts/content', 'wissen_post');
+			} elseif (in_category('denken')) {
+				get_template_part('template-parts/content', 'denken_post');
+			} elseif (in_category('handeln')) {
+				get_template_part('template-parts/content', 'handeln_post');
+			} else {
+				get_template_part( 'template-parts/content', get_post_type() );
+			}
 
-			get_template_part( 'template-parts/content', get_post_type() );
-
-			the_post_navigation();
+			// the_post_navigation();
 
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
---
+++ theme/template-parts/content-handeln_post.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Template part for displaying page content in page.php
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Agn_Theme
+ */
+
+$date = get_field('event_date');
+$location = get_field('location');
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+    <div class="header-container">
+        <header class="entry-header">
+            <div class="entry-title">
+                <h2>
+                    <?php the_title() ?>
+                </h2>
+                <p class="entry-date">
+                    <?php echo $date; ?>
+                </p>
+                <p class="entry-location">
+                    <?php echo $location; ?>
+                </p>
+            </div>
+
+            <div class="actions">
+                <a class="action has-plus" target="_blank" ref="noopener noreferrer"
+                <?php
+                $date_obj = DateTime::createFromFormat('d.m.Y', $date);
+                $gcal_query = http_build_query(array(
+                    'text' => get_the_title(),
+                    'date' => $date_obj->format('Ymd').'/'.($date_obj->format('Ymd') + 1),
+                    'details' => 'Event website: '.get_permalink(),
+                    'location' => $location
+                ));
+                echo 'href="https://calendar.google.com/calendar/r/eventedit?'.$gcal_query.'"';
+                ?>
+                >
+                    add to calendar
+                </a>
+                <a class="action" target="_blank" ref="noopener noreferrer"
+                <?php
+                echo 'href="https://www.google.com/maps/search/'.urlencode($location).'"';
+                ?>
+                >
+                    find location
+                </a>
+                <button class="action has-plus">
+                    add a new comment
+                </button>
+                <button class="action">
+                    read all
+                </button>
+            </div>
+        </header>
+    </div>
+
+    <div class="content-container">
+        <div class="entry-content">
+            <?php
+            the_content();
+
+            wp_link_pages( array(
+                'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'agn-theme' ),
+                'after'  => '</div>',
+            ) );
+            ?>
+        </div><!-- .entry-content -->
+    </div>
+</article><!-- #post-<?php the_ID(); ?> -->
